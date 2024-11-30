c0bf661950f40e84fdc23aad5a11afdb3a649c2e
MahdiLcoder <labidimahdi95@gmail.com>
2024-11-30 21:44:29 +0100

    ui design header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import AllRoutes from './Routes/AllRoutes';
-
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 function App() {
   return (
     <>
+      <Header />
       <AllRoutes />
+      <Footer />
     </>
   );
 }
--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -5,6 +5,15 @@ import MovieDetails from '../Pages/MovieDetails';
 import Search from '../Pages/Search';
 import PageNotFound from '../Pages/PageNotFound';
 
+// api key
+// 3a34276ccb3825166b2ac61cb73b2efb
+
+// https://api.themoviedb.org/3/movie/popular?api_key=3a34276ccb3825166b2ac61cb73b2efb
+// https://api.themoviedb.org/3/movie/top_rated?api_key=3a34276ccb3825166b2ac61cb73b2efb
+// https://api.themoviedb.org/3/movie/upcoming?api_key=3a34276ccb3825166b2ac61cb73b2efb
+// https://api.themoviedb.org/3/search/movie?api_key=3a34276ccb3825166b2ac61cb73b2efb&query=matrix
+// https://api.themoviedb.org/3/movie/550?api_key=3a34276ccb3825166b2ac61cb73b2efb
+
 export default function AllRoutes() {
   return (
     <Routes>
