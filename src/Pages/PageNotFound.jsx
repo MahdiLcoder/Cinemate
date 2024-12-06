beb54e19e56f9629c2fcc685351f2afc46eca5a3
MahdiLcoder <labidimahdi95@gmail.com>
2024-12-06 23:38:26 +0100

    complete notFound components

--- src/Pages/PageNotFound.jsx
+++ src/Pages/PageNotFound.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import pageNotFound from '../assets/pagenotfound.png'
+import { Link } from 'react-router-dom'
 export default function PageNotFound() {
   return (
-    <main>
+    <div className='flex justify-center items-center h-screen'>
       <section className='flex flex-col justify-center px-2'>
         <div className='flex flex-col justify-center items-center my-4'>
-          <p>404, Oops! </p>
+          <p className='text-7xl font-bold text-gray-700 dark:text-white'>404, Oops! </p>
+          <div className='max-w-6xl '>
+            <img className='rounded' src={pageNotFound} alt="404 page not found" />
+          </div>
         </div>
-        <div></div>
+        <div className='flex justify-center'>
+          <Link to='/'>
+             <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4'>Back To Cinemate</button>
+          </Link>
+        </div>
       </section>
-    </main>
+    </div>
   )
 }
